e2e758012a41df41b6df5b4d1149f6d610e682ce
Amardhruva <flashrocket@users.noreply.github.com>
2018-02-01 15:57:45 +0530

    Added new features

--- accounts/views.py
+++ accounts/views.py
@@ -16,5 +16,10 @@ class RegisterView(View):
     def post(self, request):
         form=RegistrationForm(request.POST)
         if form.is_valid:
+            user=form.save(commit=False)
+            user.is_active=False
+            user.save()
+            user.groups.add(form.cleaned_data['user_type'])
+
             return render(request, "registration/registration_success.html")
         return render(request, "registration/register.html", {"form":form})
--- baseportal/admin.py
+++ baseportal/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 # Register your models here.
+
+admin.site.site_header = 'Ejournal administration'
+
+admin.site.unregister(User)
+
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    actions = ['activate_user', 'deactivate_user']
+
+    def activate_user(self, request, queryset):
+        queryset.update(is_active=True)
+    activate_user.short_description = "Activate selected Users"
+
+    def deactivate_user(self, request, queryset):
+        queryset.update(is_active=False)
+    deactivate_user.short_description = "Deactivate selected Users"
