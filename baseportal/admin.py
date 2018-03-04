a35f0e04a665d592fd72acca49f91fcdd31db3a8
Amardhruva <flashrocket@users.noreply.github.com>
2018-03-04 09:43:11 +0530

    Added email on user activation

--- baseportal/admin.py
+++ baseportal/admin.py
@@ -1,12 +1,28 @@
+from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.urls import reverse
+
 from baseportal.models import PublishedJournal
+
 # Register your models here.
 
 admin.site.site_header = 'Ejournal administration'
 
 admin.site.unregister(User)
+
+
+def send_activated_email(request, user):
+    subject = "Your application has been accepted"
+    authorurl = request.build_absolute_uri(reverse("paperauthor:addpaper"))
+    reviewurl = request.build_absolute_uri(reverse("paperreviewer:portal"))
+    message = render_to_string("email/reviewer_selection.txt", {"user": user, "authorurl": authorurl, "reviewurl",
+                               reviewurl})
+    send_mail(subject, message, settings.ADMIN_EMAIL, [user.email])
+
 
 @admin.register(User)
 class MyUserAdmin(UserAdmin):
@@ -15,11 +31,14 @@ class MyUserAdmin(UserAdmin):
 
     def activate_user(self, request, queryset):
         queryset.update(is_active=True)
+
     activate_user.short_description = "Activate selected Users"
 
     def deactivate_user(self, request, queryset):
         queryset.update(is_active=False)
+
     deactivate_user.short_description = "Deactivate selected Users"
+
 
 @admin.register(PublishedJournal)
 class PublishedJournalAdmin(admin.ModelAdmin):
